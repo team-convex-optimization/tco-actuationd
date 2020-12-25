ec79d155c67a7dc76ba8f34b54e070fe9a6520c6
Tomasz Lisowski <t.p.lisowski@gmail.com>
2020-12-25 14:30:41 +0100

    Minor changes

--- code/io.c
+++ code/io.c
@@ -15,14 +15,14 @@ void *io_init(void)
     return io_handle;
 }
 
-int io_servo_steer(void *io_handle, uint16_t const servo_min, uint16_t const servo_max, uint8_t const ch, float const turn_frac)
+int io_servo_steer(void *io_handle, uint16_t const pulse_len_min, uint16_t const pulse_len_max, uint8_t const ch, float const turn_frac)
 {
     if (turn_frac < 0 || turn_frac > 1)
     {
         dbg_prnt_err("Turn fraction is not in the range 0 to 1.");
         return -1;
     }
-    uint16_t const duty_cycle = servo_min + ((servo_max - servo_min) * turn_frac);
+    uint16_t const duty_cycle = pulse_len_min + ((pulse_len_max - pulse_len_min) * turn_frac);
     dbg_prnt_inf("Channel %u set to duty cycle %u counts\n", ch, duty_cycle);
     if (pca9685_reg_ch_set(io_handle, ch, duty_cycle) != ERR_OK)
     {
